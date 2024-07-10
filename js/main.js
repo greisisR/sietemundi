a729860ee98103919c98f51d4eabb66bfbb5cfb7
greisisR <169100847+greisisR@users.noreply.github.com>
2024-07-10 11:40:55 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menu-btn');
+    const navbar = document.querySelector('.header .navbar');
+
+    menuBtn.addEventListener('click', function() {
+        navbar.classList.toggle('active');
+    });
+});
